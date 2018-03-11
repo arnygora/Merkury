4a74769c9f9113baf3ad9cfdf749c65a30f9d727
anthony <arnygora@gmail.com>
2018-03-11 23:49:36 +0200

    done sort users on page by online/offline parameters

--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -11,6 +11,7 @@ class UsersPage extends React.Component {
     activeUsers = userDataList.filter((user) => {
         return user.active;
     });
+
     constructor() {
         super();
         let exampleItems = this.activeUsers.map((item, index) => {
@@ -21,35 +22,50 @@ class UsersPage extends React.Component {
             exampleItems: exampleItems,
             pageOfItems: [],
             activeUsers: [],
-    };
+        };
     }
+
     onChangePage = (pageOfItems) => {
-        this.setState({ pageOfItems: pageOfItems });
+        this.setState({pageOfItems: pageOfItems});
     };
 
     //sort users by filter (select button)
-    onFilterUsers = (e) => {
-        if(e === "Offline") {
-           let Users = userDataList.filter((user) => {
-                return !user.active;
-            });
-        } else if (e === "Active first") {
-            let Users = userDataList.filter((user) => {
-                return user.active;
-            });
+    FilterUsers = (e) => {
+        if (e === "Offline") {
+            this.setState({
+                exampleItems: userDataList.filter((user) => {
+                    return !user.active;
+                })
+            })
         }
+        else if (e === "Active first") {
+            this.setState({
+                exampleItems: userDataList.filter((user) => {
+                    return user.active;
+                })
+            })
+        } else {
+            this.setState({
+                exampleItems: userDataList.map((user, index) => {
+                    return user;
+
+                })
+            })
+        }
+
         console.log(e);
     };
+
     render() {
         let UserSort = ["Active first", "All users", "Offline"];
-        let userCount = this.activeUsers.length;
+        let userCount = this.state.exampleItems.length;
         return (
             <div>
                 <Header>
                     <section>
                         <div className="d-flex justify-content-between align-items-center">
                             <h4>Users <span className="text-secondary">({userCount})</span></h4>
-                            <Select data={UserSort} onSortByFilter={this.onFilterUsers}/>
+                            <Select data={UserSort} onSortByFilter={this.FilterUsers}/>
                         </div>
                         <ul>
                             <li className="row text-secondary tableHeader">
@@ -60,12 +76,13 @@ class UsersPage extends React.Component {
                                 <div className="col-lg-1"></div>
                             </li>
                         </ul>
-                        <UsersList data={this.state.pageOfItems} />
-                        <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage} />
+                        <UsersList data={this.state.pageOfItems}/>
+                        <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage}/>
                     </section>
                 </Header>
             </div>
         );
     }
 }
+
 export default UsersPage;
