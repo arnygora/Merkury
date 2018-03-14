fc83d73822d7649f19968065b34f3bddd2051690
anthony <arnygora@gmail.com>
2018-03-14 19:27:37 +0200

    fix display active/offline users

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -14,7 +14,13 @@ class UsersList extends React.Component {
                                         <div className={item.divForPhoto}></div>
                                         <p className="small pl-3 mb-0">{item.userName}<span className="d-block text-secondary">{item.position}</span></p>
                                     </div>
-                                    <div className="col-md-2 col-xl-2 activity"><span className="far fa-clock">{' ' + item.status}</span></div>
+                                    <div className="col-md-2 col-xl-2 activity">
+                                    {
+                                        item.active ? <span className="far fa-clock text-success">{' ' + item.status}</span> :
+                                    	<span className="far fa-clock">{' ' + item.status}</span>
+                                    }
+                                        {/*<span className={item.active ? 'text-success' : null}>{' ' + item.status}</span>*/}
+                                	</div>
                                     <div className="col-12 col-sm-6 col-md-2 col-xl-2 mail">{item.email}</div>
                                     <div className="col-12 col-sm-6 col-md-2 col-xl-2 phone">{item.phoneNumber}</div>
                                     <div className="col-1 dotLink">
